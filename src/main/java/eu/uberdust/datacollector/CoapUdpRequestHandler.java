7d33c7b5893f1a082184690e7081c1d7c856f967
amaxilat <amaxilat@cti.gr>
2012-05-19 20:43:10 +0300

    working on coap

--- src/main/java/eu/uberdust/datacollector/CoapUdpRequestHandler.java
+++ src/main/java/eu/uberdust/datacollector/CoapUdpRequestHandler.java
@@ -29,13 +29,14 @@ public class CoapUdpRequestHandler implements Runnable {
     @Override
     public void run() {
         byte[] data = packet.getData();
+        int len = packet.getLength();
         StringBuilder packet2String = new StringBuilder("request:");
         for (byte b : data) {
             packet2String.append(Byte.toString(b)).append("|");
         }
         LOGGER.info(packet2String.toString());
         final Integer[] macAddress = new Integer[2];
-        String destination = "417";
+        String destination = "472";
         if (destination.length() == 4) {
             macAddress[0] = Integer.valueOf(destination.substring(0, 2), 16);
             macAddress[1] = Integer.valueOf(destination.substring(2, 4), 16);
@@ -43,12 +44,24 @@ public class CoapUdpRequestHandler implements Runnable {
             macAddress[0] = Integer.valueOf(destination.substring(0, 1), 16);
             macAddress[1] = Integer.valueOf(destination.substring(1, 3), 16);
         }
+
+        LOGGER.info("here");
         final XBeeAddress16 address16 = new XBeeAddress16(macAddress[0], macAddress[1]);
-        int[] bytes = new int[packet.getData().length];
+        LOGGER.info("here1");
+
+        int[] bytes = new int[len+ 1];
         bytes[0] = 51;
-        for (int i = 0; i < packet.getData().length; i++) {
-            bytes[i + 1] = packet.getData()[i];
+        for (int i = 0; i < len; i++) {
+            short read = (short) ((short) data[i] & 0xff);
+            bytes[i + 1] = read;
         }
+
+        StringBuilder message = new StringBuilder("Requesting:");
+        for (int i = 0; i < len; i++) {
+            message.append(bytes[i]).append("|");
+        }
+
+        LOGGER.info(message.toString());
         try {
             LOGGER.info("sending to device");
             XBeeRadio.getInstance().send(address16, 112, bytes);
