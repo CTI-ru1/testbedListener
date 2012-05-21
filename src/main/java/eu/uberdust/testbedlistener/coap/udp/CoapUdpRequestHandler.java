c4c03324e9a8f6f5c2ab4e39133ee46aac1097d5
amaxilat <amaxilat@cti.gr>
2012-05-22 01:36:25 +0300

    pmd and minor code fixes

--- src/main/java/eu/uberdust/testbedlistener/coap/udp/CoapUdpRequestHandler.java
+++ src/main/java/eu/uberdust/testbedlistener/coap/udp/CoapUdpRequestHandler.java
@@ -13,95 +13,89 @@ import java.net.DatagramPacket;
 import java.util.List;
 
 /**
- * Created by IntelliJ IDEA.
- * User: amaxilatis
- * Date: 5/19/12
- * Time: 7:36 PM
- * To change this template use File | Settings | File Templates.
+ * Handler Thread fro the COAP upd packets of the server.
  */
-public class CoapUdpRequestHandler implements Runnable {
+public class CoapUdpRequestHandler implements Runnable {//NOPMD
 
     /**
      * LOGGER.
      */
     private static final Logger LOGGER = Logger.getLogger(CoapUdpRequestHandler.class);
 
-    private DatagramPacket packet;
+    private transient final DatagramPacket packet;
 
-    public CoapUdpRequestHandler(DatagramPacket packet) {
+    /**
+     * Constructor.
+     *
+     * @param packet The UDP packet of the request.
+     */
+    public CoapUdpRequestHandler(final DatagramPacket packet) {
         this.packet = packet;
     }
 
     @Override
     public void run() {
-        byte[] adata = packet.getData();
-        int myLen = adata.length;
-        for (int i = adata.length - 1; i >= 0; i--) {
-            if (adata[i] == 0) {
-                myLen = i;
-            }
-        }
-        LOGGER.info(myLen);
-        byte[] adata2 = new byte[myLen];
-        System.arraycopy(adata, 0, adata2, 0, myLen);
-        LOGGER.info(adata.length);
-        Message mess = Message.fromByteArray(adata2);
-        Message request = new Message(mess.getType(), mess.getCode());
-        request.setMID(mess.getMID());
-        request.setPayload(mess.getPayload());
-        LOGGER.info(mess.getPayload().length);
-//        for (Option option : mess.getOptions()) {
-//
-//            if (option.getIntValue() != OptionNumberRegistry.URI_PATH) {
-//                LOGGER.info("1");
-//                request.addOption(option);
-//            }
-//        }
-        final String uriPath = mess.getUriPath();
+        final byte[] inData = cleanupData(packet.getData());
+        final Message updRequest = Message.fromByteArray(inData);
+
+        final Message coapRequest = new Message(updRequest.getType(), updRequest.getCode());
+
+        //set MID
+        coapRequest.setMID(updRequest.getMID());
+
+        //set PAYLOAD
+        coapRequest.setPayload(updRequest.getPayload());
+
+        //set URI_PATH
+        final String uriPath = updRequest.getUriPath();
         String nodeUrn = uriPath.substring(1, uriPath.indexOf("/", 1));
         String newUri = uriPath.substring(uriPath.indexOf("/", 1));
-        LOGGER.info(newUri);
         List<Option> uriPatha = Option.split(OptionNumberRegistry.URI_PATH, newUri, "/");
-        request.setOptions(OptionNumberRegistry.URI_PATH, uriPatha);
-        List<Option> uriPathb = Option.split(OptionNumberRegistry.URI_QUERY, mess.getQuery(), "&");
-        request.setOptions(OptionNumberRegistry.URI_QUERY, uriPathb);
-        LOGGER.info("OptionCount" + request.getOptionCount());
+        coapRequest.setOptions(OptionNumberRegistry.URI_PATH, uriPatha);
 
-        LOGGER.info(mess.getUriPath());
-        LOGGER.info(nodeUrn);
+        //SET URI_QUERY
+        List<Option> uriPathb = Option.split(OptionNumberRegistry.URI_QUERY, updRequest.getQuery(), "&");
+        coapRequest.setOptions(OptionNumberRegistry.URI_QUERY, uriPathb);
+
+        LOGGER.info("UDP uriPath: " + updRequest.getUriPath());
+        LOGGER.info("UDP uriQuery: " + updRequest.getQuery());
+        LOGGER.info("COAP uriPath: " + coapRequest.getUriPath());
+        LOGGER.info("COAP uriQuery: " + coapRequest.getQuery());
+
+        LOGGER.info("NodeUrn: " + nodeUrn);
 
         //= packet.getData();
         //len = packet.getLength();
-        byte[] data = request.toByteArray();
+        final byte[] data = coapRequest.toByteArray();
         int len = data.length;
         if (nodeUrn.contains("0x")) {
             nodeUrn = nodeUrn.substring(nodeUrn.indexOf("0x") + 2);
 
-            int[] bytes = new int[len + 1];
+            final int[] bytes = new int[len + 1];
             bytes[0] = 51;
             for (int i = 0; i < len; i++) {
                 short read = (short) ((short) data[i] & 0xff);
                 bytes[i + 1] = read;
             }
 
-            StringBuilder messageBinary = new StringBuilder("Requesting[Bytes]:");
+            final StringBuilder messageBinary = new StringBuilder("Requesting[Bytes]:");
             for (int i = 0; i < len + 1; i++) {
                 messageBinary.append(bytes[i]).append("|");
             }
             LOGGER.info(messageBinary.toString());
 
             final int[] macAddress = Converter.AddressToInteger(nodeUrn);
-            CoapServer.getInstance().addRequest(nodeUrn, request);
+            CoapServer.getInstance().addRequest(nodeUrn, coapRequest);
             final XBeeAddress16 address16 = new XBeeAddress16(macAddress[0], macAddress[1]);
 
             try {
                 LOGGER.info("sending to device");
                 XBeeRadio.getInstance().send(address16, 112, bytes);
             } catch (Exception e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                LOGGER.error(e.getMessage(), e);
             }
         } else {
-            LOGGER.info("reply from uberdust!");
+            LOGGER.info("reply from uberdust! Device has no mac address");
 
             Message message = new Message();
             message.setPayload("urn:node:capability:card");
@@ -114,5 +108,24 @@ public class CoapUdpRequestHandler implements Runnable {
 
             CoapServer.getInstance().sendReply(replyPacket);
         }
+    }
+
+    /**
+     * Clear tailing zeros from the udpPacket.
+     *
+     * @param data the incoming packet.
+     * @return the data from the udp packet without tailing zeros.
+     */
+    private byte[] cleanupData(final byte[] data) {
+        int myLen = data.length;
+        for (int i = data.length - 1; i >= 0; i--) {
+            if (data[i] == 0) {
+                myLen = i;
+            }
+        }
+        final byte[] adata = new byte[myLen];
+        System.arraycopy(data, 0, adata, 0, myLen);
+        LOGGER.info(adata.length);
+        return adata;
     }
 }
