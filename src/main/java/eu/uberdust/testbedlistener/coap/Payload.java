56d5d73814bf85897f9751336a4a0007e0d3e3de
amaxilat <amaxilat@cti.gr>
2012-12-01 15:51:00 +0200

    merge changes

---
+++ src/main/java/eu/uberdust/testbedlistener/coap/Payload.java
@@ -0,0 +1,35 @@
+package eu.uberdust.testbedlistener.coap;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: amaxilatis
+ * Date: 11/22/12
+ * Time: 12:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Payload {
+    byte[] bytes;
+
+    public Payload(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Payload payload = (Payload) o;
+
+        if (!Arrays.equals(bytes, payload.bytes)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(bytes);
+    }
+}
